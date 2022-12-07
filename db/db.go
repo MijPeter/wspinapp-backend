8154638c32a2e22d9217b2ce240309037060c05c
Michal Jankowski <37120577+MijPeter@users.noreply.github.com>
2022-12-07 02:40:33 +0100

    Postgres (#4)
    * add postgres to docker-compose and add driver for db

---
+++ db/db.go
@@ -0,0 +1,44 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+)
+
+var DB *sql.DB
+
+const (
+	host     = "wspinapp_db"
+	dbPort   = 5432
+	user     = "wspinapp"
+	password = "sprayitwhileyoucanmyfriend"
+	dbname   = "wspinapp"
+)
+
+func ConnectDb() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, dbPort, user, password, dbname)
+	var err error
+	DB, err = sql.Open("postgres", psqlInfo)
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = DB.Ping()
+	if err != nil {
+		DB.Close()
+		panic(err)
+	}
+}
+
+func DisconnectDb() {
+	err := DB.Close()
+
+	if err != nil {
+		panic(err)
+	}
+
+}
