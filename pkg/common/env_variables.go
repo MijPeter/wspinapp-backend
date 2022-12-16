fec1b1ef57009e1ed54b0912bace11d15bbe8090
MijPeter <michal.kuba.jankowski@gmail.com>
2022-12-16 18:45:39 +0100

    finished :)

--- pkg/common/env_variables.go
+++ pkg/common/env_variables.go
@@ -1,18 +1,12 @@
 package common
 
 import (
-	"github.com/joho/godotenv"
 	"log"
 	"os"
+	"strconv"
 )
 
-func LoadEnvironmentVariables() {
-	err := godotenv.Load()
-
-	if err != nil {
-		log.Fatalf("Couldn't load environment variables. Exiting. %s\n", err.Error())
-	}
-}
+// TODO if this is to stay then probably use map or enum and not plain strings
 
 func EnvCloudName() string {
 	return os.Getenv("CLOUDINARY_CLOUD_NAME")
@@ -28,4 +22,30 @@ func EnvCloudAPISecret() string {
 
 func EnvCloudUploadFolder() string {
 	return os.Getenv("CLOUDINARY_UPLOAD_FOLDER")
+}
+
+func EnvDBUser() string {
+	return os.Getenv("POSTGRES_USER")
+}
+
+func EnvDBPassword() string {
+	return os.Getenv("POSTGRES_PASSWORD")
+}
+
+func EnvDBName() string {
+	return os.Getenv("POSTGRES_DB")
+}
+
+func EnvDBHost() string {
+	return os.Getenv("POSTGRES_HOST")
+}
+
+func EnvDBPort() int {
+	ret, err := strconv.Atoi(os.Getenv("POSTGRES_PORT"))
+
+	if err != nil {
+		log.Fatalln("Couldn't load port number for db from environment variable.")
+	}
+
+	return ret
 }
