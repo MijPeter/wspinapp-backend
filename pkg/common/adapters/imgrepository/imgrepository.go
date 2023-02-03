af3e899fbd6764d521529f432efde07478e83f76
Michał Jankowski <michal.kuba.jankowski@gmail.com>
2023-02-03 22:35:58 +0100

    add cron job that removes dangling images, this makes it very unsecure to run code locally :)

--- pkg/common/adapters/imgrepository/imgrepository.go
+++ pkg/common/adapters/imgrepository/imgrepository.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"example/wspinapp-backend/pkg/common"
 	"github.com/cloudinary/cloudinary-go/v2"
+	"github.com/cloudinary/cloudinary-go/v2/api/admin"
 	"github.com/cloudinary/cloudinary-go/v2/api/uploader"
 	"log"
 	"time"
@@ -16,9 +17,16 @@ type cloudinaryConfig struct {
 	uploadFolder string
 }
 
+type Asset struct {
+	ID  string
+	Url string
+}
+
 type ImageRepository interface {
 	Upload(input interface{}) (string, error)
 	// RemoteUpload(url Url) (string, error) https://dev.to/hackmamba/robust-media-upload-with-golang-and-cloudinary-gin-gonic-version-54ii
+	Assets() ([]Asset, error)
+	DeleteAssets(assetsID []string) error
 }
 
 // implementation of ImageRepository
@@ -29,7 +37,7 @@ type cloudinaryRepository struct {
 
 // Upload uploads validated image file to image repository
 func (imageStore *cloudinaryRepository) Upload(input interface{}) (string, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second) // todo the fuck is that?
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	//upload file
@@ -38,6 +46,37 @@ func (imageStore *cloudinaryRepository) Upload(input interface{}) (string, error
 		return "", err
 	}
 	return uploadParam.SecureURL, nil
+}
+
+// TODO delete assets from wall folder only
+func (imageStore *cloudinaryRepository) Assets() ([]Asset, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	var resources []Asset
+	assets, err := imageStore.cloudinary.Admin.Assets(ctx, admin.AssetsParams{MaxResults: 500})
+	if err != nil {
+		return nil, err
+	}
+
+	for _, asset := range assets.Assets {
+		resources = append(resources, Asset{ID: asset.PublicID, Url: asset.SecureURL})
+	}
+
+	return resources, nil
+}
+
+func (imageStore *cloudinaryRepository) DeleteAssets(assetsID []string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if len(assetsID) > 100 {
+		log.Println("Too many assets to delete, choosing only 100 to delete") // cloudinary lets us delete only up to 100 assets at once
+		assetsID = assetsID[0:100]
+	}
+
+	_, err := imageStore.cloudinary.Admin.DeleteAssets(ctx, admin.DeleteAssetsParams{PublicIDs: assetsID})
+	return err
 }
 
 // New creates a cloudinary config (from environment variables file) and connects to cloudinary.
---
+++ pkg/services/cron_service/image_cron.go
@@ -0,0 +1,74 @@
+package cron_service
+
+import (
+	"example/wspinapp-backend/pkg/common/adapters/imgrepository"
+	"gorm.io/gorm"
+	"log"
+	"time"
+)
+
+const HOURS = 24 // Run is activated every HOURS
+
+type CronService struct {
+	database        *gorm.DB
+	imageRepository imgrepository.ImageRepository
+}
+
+func New(db *gorm.DB, imageRepository imgrepository.ImageRepository) CronService {
+	return CronService{database: db, imageRepository: imageRepository}
+}
+
+func (c *CronService) Run() {
+	// in theory should be run at 3 am, but this is not that important right now
+	for true {
+		log.Println("DING DING DING, running deleteAllDanglingImages cron job")
+		c.deleteAllDanglingImages()
+		time.Sleep(HOURS * time.Hour)
+	}
+}
+
+func (c *CronService) deleteAllDanglingImages() {
+	var activeImageUrls, imageUrls, imagePreviewUrls []string
+	c.database.Select("image_url").Table("walls").Find(&imageUrls)
+	c.database.Select("image_preview_url").Table("walls").Find(&imagePreviewUrls)
+
+	activeImageUrls = append(imageUrls, imagePreviewUrls...)
+
+	log.Println("Active images that won't be deleted:")
+
+	for _, img := range activeImageUrls {
+		log.Println(img)
+	}
+
+	imgRepositoryAssets, err := c.imageRepository.Assets()
+	if err != nil {
+		log.Printf("Couldn't list assets. Reason\n%s", err.Error()) // some more serious log here
+		return
+	}
+	log.Println("Printing all assets:")
+	for _, asset := range imgRepositoryAssets {
+		log.Println(asset)
+	}
+
+	activeImageUrlsMap := make(map[string]struct{})
+	for _, activeImageUrl := range activeImageUrls {
+		activeImageUrlsMap[activeImageUrl] = struct{}{}
+	}
+
+	var toBeDeletedAssetsID []string
+	for _, asset := range imgRepositoryAssets {
+		_, exists := activeImageUrlsMap[asset.Url]
+		if !exists {
+			toBeDeletedAssetsID = append(toBeDeletedAssetsID, asset.ID)
+		}
+	}
+
+	log.Printf("About to delete %d assets", len(toBeDeletedAssetsID))
+
+	err = c.imageRepository.DeleteAssets(toBeDeletedAssetsID)
+	if err != nil {
+		log.Printf("Failed to delete assets: Reason\n%s", err.Error())
+	} else {
+		log.Println("Successfuly deleted assets :)")
+	}
+}
