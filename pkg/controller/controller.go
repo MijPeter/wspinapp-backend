7534bbbd7a666f9463fe6005cf7e0d397e6c3de7
Michał Jankowski <michal.kuba.jankowski@gmail.com>
2023-08-20 13:16:52 +0200

    Add monitoring for time metrics

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -4,7 +4,10 @@ import (
 	"example/wspinapp-backend/pkg/services"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"strconv"
+	"time"
 )
 
 type routesHandler struct {
@@ -17,6 +20,36 @@ func prometheusHandler() gin.HandlerFunc {
 
 	return func(c *gin.Context) {
 		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
+func attachMetrics() gin.HandlerFunc {
+	buckets := []float64{.005, .01, .025, .05, .1, .25, .5, 1, 2.5, 5, 10}
+
+	responseTimeHistogram := prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Namespace: "namespace",
+		Name:      "http_server_request_duration_seconds",
+		Help:      "Histogram of response time for handler in seconds",
+		Buckets:   buckets,
+	}, []string{"route", "method", "status_code"})
+
+	prometheus.MustRegister(responseTimeHistogram)
+
+	return func(c *gin.Context) {
+		start := time.Now()
+
+		// Let the request go through the handler chain
+		c.Next()
+
+		// Now calculate the metrics after the handler has finished processing
+		duration := time.Since(start)
+
+		// Get route (path), method and status code from the current context
+		route := c.Request.URL.Path // Or use c.FullPath() for the matched route pattern
+		method := c.Request.Method
+		statusCode := strconv.Itoa(c.Writer.Status())
+
+		responseTimeHistogram.WithLabelValues(route, method, statusCode).Observe(duration.Seconds())
 	}
 }
 
@@ -34,7 +67,7 @@ func RegisterRoutes(r *gin.Engine, service services.WebService) {
 	router := r.Group("/walls")
 	router.Use(gin.BasicAuth(gin.Accounts{
 		"wspinapp": "wspinapp",
-	}))
+	}), attachMetrics())
 
 	router.POST("", h.AddWall)
 	router.GET("", h.GetWalls)
