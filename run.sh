1e2aa2b4e9d0c5255598e762c8c7b44264500001
Michał Jankowski <michal.kuba.jankowski@gmail.com>
2023-09-10 15:54:03 +0200

    update run script

--- run.sh
+++ run.sh
@@ -7,24 +7,29 @@ Usage: $0 [OPTION]
 Run the server in different modes.
 
 Options:
+    --local                     Run the server in normal mode.
     --test                      Run the server in test mode.
     --fast                      Run tests without bringing up all services.
     --generate-goldens          Re-generate golden files when tests don't match.
     --migrate migration_name    Run the migration tool for a given migration name.
 
-No option will run the server in normal mode.
+Run without options or with '-h' or '--help' to display this help message.
 EOF
 }
 
-# Check for no arguments and run the server in normal mode
+# Check for no arguments and show the help message
 if [ "$#" -eq 0 ]; then
-    docker compose up --build --remove-orphans -d
+    show_help
     exit 0
 fi
 
 # Check passed arguments
 while [ "$#" -gt 0 ]; do
     case "$1" in
+        --local)
+            docker compose up --build --remove-orphans -d
+            shift
+            ;;
         --test)
             docker compose -f docker-compose.test.yml up --build --remove-orphans --abort-on-container-exit
             shift
